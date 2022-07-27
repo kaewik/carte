b5e2d7314ecfa78af059e90ef0dc2890f4ca1bda
lugggas <32685131+lugggas@users.noreply.github.com>
2022-07-28 00:46:37 +0200

    Remove template functionality from project

--- carte-rest/src/Application.hs
+++ carte-rest/src/Application.hs
@@ -8,7 +8,6 @@ module Application where
 import Foundation
 import Yesod.Core
 
-import Add
 import Home
 
 mkYesodDispatch "App" resourcesApp
--- carte-rest/src/Home.hs
+++ carte-rest/src/Home.hs
@@ -9,8 +9,5 @@ getHomeR :: Handler Html
 getHomeR = defaultLayout $ do
     setTitle "Minimal Multifile"
     [whamlet|
-        <p>
-            <a href=@{AddR 5 7}>HTML addition
-        <p>
-            <a href=@{AddR 5 7}?_accept=application/json>JSON addition
+        <h1> You are on my Website!</h1>
     |]
