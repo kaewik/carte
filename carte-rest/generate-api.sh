aed8e4421cd075edaf16392363ef4b004adfa6c0
lugggas <32685131+lugggas@users.noreply.github.com>
2022-07-28 00:46:38 +0200

    Make open-api generation not mess up my project

--- carte-rest/generate-api.sh
+++ carte-rest/generate-api.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+DIRNAME=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
 echo 'Generate REST-API lib ...'
-docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli generate \
+docker run --rm -v "${DIRNAME}:/local" openapitools/openapi-generator-cli generate \
     -i /local/carte-spec.yaml \
     -g haskell \
     -o /local/open-api/haskell
